2790395ce524c6543310da67204bf71f493a1f59
Brian121301 <briannajera3@gmail.com>
2026-05-12 19:05:53 -0500

    fixed issue with resume button

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -6,68 +6,77 @@ import { TypeAnimation } from "react-type-animation";
 
 const HeroSection = () => {
     return (
-        <section>
-            <div className=' grid grid-cols-1 lg:grid-cols-12'>
-                <motion.div
-                    initial={{ opacity: 0, scale: 0 }}
-                    animate={{ opacity: 1, scale: 1 }}
-                    transition={{ duration: 0.5 }}
-                    className='col-span-7 place-self-center text-center sm:text-left sm:justify-self-start mb-4'>
-                    <h1 className='mb-4 text-6xl text-white lg:text-8xl font-extrabold'>
-                        Hi, I'm
-                    </h1>
-                    <h1
-                        className='bg-clip-text text-transparent bg-gradient-to-r from-[#a2d6e5] to-[#bdb8b8] mb-4 text-6xl lg:text-8xl font-extrabold'>
-                        Brian Najera
-                    </h1>
-                    <TypeAnimation
-                        sequence={[
-                            "Web Developer",
-                            1000,
-                            "Mobile Developer",
-                            1000,
-                            "Cloud Practitioner",
-                            1000,
-                        ]}
-                        wrapper="span"
-                        speed={50}
-                        className='text-4xl font-semibold bg-clip-text text-transparent bg-gradient-to-r from-[#a2d6e5] to-[#bdb8b8] sm:mt-0'
-                        repeat={Infinity}
-                    />
-                    <div>
-                        <button onClick={() => {
-                            const contactSection = document.getElementById("contact");
-                            if (contactSection) {
-                                contactSection.scrollIntoView({ behavior: "smooth" });
-                            }
-                        }}
-                            className='px-6 py-4 rounded-full w-full sm:w-fit mr-4 bg-[#00A8CC] hover:bg-slate-200 text-black sm:mt-0 mt-4'>
-                            Hire Me
-                        </button>
-                        <button
-                            className='px-6 py-4 rounded-full w-full sm:w-fit mt-4 bg-[#FF6F61] hover:bg-slate-200 text-black'
-                            onClick={() => window.open("/Brian-Najera-Resume.pdf", "_blank")}>View Resume</button>
-                    </div>
-                </motion.div>
-
-                <motion.div
-                    initial={{ opacity: 0, scale: 0 }}
-                    animate={{ opacity: 1, scale: 1 }}
-                    transition={{ duration: 0.5 }}
-                    className='col-span-5 place-self-center mt-10 lg:mt-0'>
-                    <div className='w-[700px] h-[700px] sm:[500px] sm:[500px] p-10 relative'>
-                        <Image
-                            src="/images/sitting2.jpg"
-                            alt="hero image"
-                            className="rounded-full absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-                            width={700}
-                            height={700}
-                        />
-                    </div>
-                </motion.div>
+      <section>
+        <div className=" grid grid-cols-1 lg:grid-cols-12">
+          <motion.div
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 0.5 }}
+            className="col-span-7 place-self-center text-center sm:text-left sm:justify-self-start mb-4"
+          >
+            <h1 className="mb-4 text-6xl text-white lg:text-8xl font-extrabold">
+              Hi, I'm
+            </h1>
+            <h1 className="bg-clip-text text-transparent bg-gradient-to-r from-[#a2d6e5] to-[#bdb8b8] mb-4 text-6xl lg:text-8xl font-extrabold">
+              Brian Najera
+            </h1>
+            <TypeAnimation
+              sequence={[
+                "Web Developer",
+                1000,
+                "Mobile Developer",
+                1000,
+                "Cloud Practitioner",
+                1000,
+              ]}
+              wrapper="span"
+              speed={50}
+              className="text-4xl font-semibold bg-clip-text text-transparent bg-gradient-to-r from-[#a2d6e5] to-[#bdb8b8] sm:mt-0"
+              repeat={Infinity}
+            />
+            <div>
+              <button
+                onClick={() => {
+                  const contactSection = document.getElementById("contact");
+                  if (contactSection) {
+                    contactSection.scrollIntoView({ behavior: "smooth" });
+                  }
+                }}
+                className="px-6 py-4 rounded-full w-full sm:w-fit mr-4 bg-[#00A8CC] hover:bg-slate-200 text-black sm:mt-0 mt-4"
+              >
+                Hire Me
+              </button>
+              <a
+                href="/Brian-Najera-Resume.pdf"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <button className="px-6 py-4 rounded-full w-full sm:w-fit mt-4 bg-[#FF6F61] hover:bg-slate-200 text-black">
+                  View Resume
+                </button>
+              </a>
             </div>
-        </section>
-    )
+          </motion.div>
+
+          <motion.div
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 0.5 }}
+            className="col-span-5 place-self-center mt-10 lg:mt-0"
+          >
+            <div className="w-[700px] h-[700px] sm:[500px] sm:[500px] p-10 relative">
+              <Image
+                src="/images/sitting2.jpg"
+                alt="hero image"
+                className="rounded-full absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+                width={700}
+                height={700}
+              />
+            </div>
+          </motion.div>
+        </div>
+      </section>
+    );
 }
 
 export default HeroSection
